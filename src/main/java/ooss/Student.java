24b3371385f78741377a72248dbfa76dadf024f0
MIKEARCE PASCUAL (PLAN-IRIS-ISD-OOCLL/MNL) <PASCUMI3@OOCL.COM>
2023-08-16 20:29:30 +0800

    feat: implemented should_return_result_by_student_is_in_class_when_isIn_given_student

--- src/main/java/ooss/Student.java
+++ src/main/java/ooss/Student.java
@@ -2,11 +2,21 @@ package ooss;
 
 public class Student extends Person {
 
+    private Klass klass;
+
     public Student(int id, String name, int age) {
         super(id, name, age);
     }
 
     public String introduce(){
         return super.introduce().concat(" I am a student.");
+    }
+
+    public void join(Klass klass) {
+        this.klass = klass;
+    }
+
+    public boolean isIn(Klass klass) {
+        return klass.equals(this.klass);
     }
 }
