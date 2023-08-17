f5f70729eefdb0c1316b49f32ef8e5d18c6d352d
MIKEARCE PASCUAL (PLAN-IRIS-ISD-OOCLL/MNL) <PASCUMI3@OOCL.COM>
2023-08-17 08:45:22 +0800

    fix: updated introduce method in Student.java to implement should_return_message_with_name_and_age_when_introduce_given_student and should_return_is_equals_by_id_when_equals_given_two_students_with_id

--- src/main/java/ooss/Student.java
+++ src/main/java/ooss/Student.java
@@ -9,16 +9,7 @@ public class Student extends Person {
     }
 
     public String introduce(){
-        StringBuilder introduce = new StringBuilder();
-        introduce.append(super.introduce()
-                .concat(String.format(" I am a student.")));
-        if(this.klass.isLeader(this)){
-            introduce.append(String.format(" I am the leader of class %d.",this.klass.getNumber()));
-        }
-        else{
-            introduce.append(String.format(" I am in class %d.",this.klass.getNumber()));
-        }
-        return introduce.toString();
+        return super.introduce().concat(" I am a student.");
     }
 
     public void join(Klass klass) {
